8225a459b817c18704700a54e3ca1d593558419e
amRadhwen <radhweneamri9631@gmail.com>
2022-04-03 17:33:51 +0100

    ready

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='home-page'>
-        <h1>Home Page</h1>
+        <div className="title4">
+          <h2 className="title-inner4">trending products</h2>
+          <div className="line">
+            <span></span>
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/pages/Notification.jsx
+++ src/components/pages/Notification.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Notification() {
   return (
     <div className='notification-page'>
-        <h1>Notifications</h1>
+        <div className="title1">
+            <h4>Recent</h4>
+            <h2 className="title-inner1">Notifications</h2>
+        </div>
     </div>
   )
 }
