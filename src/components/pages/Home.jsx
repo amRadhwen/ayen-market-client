336e0999e2fcb2b957e6cd03455aecbc20eecedd
amRadhwen <radhweneamri9631@gmail.com>
2022-04-03 17:39:02 +0100

    ready

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='home-page'>
         <div className="title4">
-          <h2 className="title-inner4">trending products</h2>
+          <h2 className="title-inner4">Home</h2>
           <div className="line">
             <span></span>
           </div>
